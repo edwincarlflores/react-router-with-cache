79c962b7f47f9bae1aff99a084bacfc923a2a998
Edwin Carl Flores <edwincarlflores@gmail.com>
2022-10-20 03:27:09 +0800

    Create post service

---
+++ src/services/posts.ts
@@ -0,0 +1,19 @@
+import fetchAPI from "../utils/api";
+
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+export type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+type FetchPosts = () => Promise<Post[]>;
+
+type FetchPostById = (id: number) => Promise<Post>;
+
+export const fetchPosts: FetchPosts = () => fetchAPI(`${BASE_URL}/posts`);
+
+export const fetchPostById: FetchPostById = (id) =>
+  fetchAPI(`${BASE_URL}/posts/${id}`);
